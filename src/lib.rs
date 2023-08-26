d5391722bf643402a6c76044d04695240e36028a
okirshen <okirshen@gmail.com>
2023-08-26 01:18:58 +0100

    refactor!: Rename lib and function to `randsort`
    BREAKING CHANGE: sort function renamed.
    this is more catch

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 use rand::prelude::*;
 
 pub trait RandSort<T> {
-    fn rand_sort(&mut self);
+    fn randsort(&mut self);
 }
 
 impl<T: PartialOrd> RandSort<T> for Vec<T> {
-    fn rand_sort(&mut self) {
+    fn randsort(&mut self) {
         let mut rng = rand::thread_rng();
         loop {
             self.shuffle(&mut rng);
@@ -30,7 +30,7 @@ mod tests {
     #[test]
     fn test() {
         let mut vec = vec![5, 4, 8, 9, 12, 1, 3909, 567, 5, 6];
-        vec.rand_sort();
+        vec.randsort();
         assert_eq!(vec, [1, 4, 5, 5, 6, 8, 9, 12, 567, 3909]);
     }
 }
